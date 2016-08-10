6cd1b7a6e2234d85090c7d479321efcd5ef8f07d
Abhishek Dutt <abhishekdutt@outlook.com>
2016-08-10 17:04:41 +0530

    Minor

--- die lines/cannyTest.lua
+++ die lines/cannyTest.lua
@@ -10,6 +10,7 @@ require 'cv.videoio' -- Video input/output
 loadType = cv.IMREAD_UNCHANGED
 img = cv.imread{'images/IMG20160623164342.jpg', loadType}
 img = cv.imread{'images/IMG20160623164432.jpg', loadType}
+img = cv.imread{'pro.png', loadType}
 -- image.display(img1)
 local edges = torch.ByteTensor(img:size()[1], img:size()[2])
 img2 = cv.Canny{image=img, edges=edges, threshold1=10, threshold2=40}
